7fc36f7464f293859e7f03519d2f60c55c92af10
Cyber_001 <144014192+Asaiba@users.noreply.github.com>
2025-02-21 20:30:06 +0200

    Upd

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,29 +11,29 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *new_str;
-    int i, j, len1 = 0, len2 = 0;
+	char *new_str;
+	int i, j, len1 = 0, len2 = 0;
 
-    if (s1 != NULL)
-    {
-        while (s1[len1])
-            len1++;
-    }
-    if (s2 != NULL)
-    {
-        while (s2[len2])
-            len2++;
-    }
+	if (s1 != NULL)
+	{
+		while (s1[len1])
+			len1++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[len2])
+			len2++;
+	}
 
-    new_str = malloc(sizeof(char) * (len1 + len2 + 1));
-    if (new_str == NULL)
-        return (NULL);
+	new_str = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (new_str == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        new_str[i] = s1[i];
-    for (j = 0; j < len2; j++, i++)
-        new_str[i] = s2[j];
-    new_str[i] = '\0';
+	for (i = 0; i < len1; i++)
+		new_str[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		new_str[i] = s2[j];
+	new_str[i] = '\0';
 
-    return (new_str);
+	return (new_str);
 }
